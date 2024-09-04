87d5e6a8a12eb5a6b75a2a8591b20875be4e22af
Tomas-Perez <164664659+Tomas-Perez@users.noreply.github.com>
2024-09-03 23:30:28 -0300

    Servidor FS

--- filesystem/src/filesystem.c
+++ filesystem/src/filesystem.c
@@ -10,32 +10,47 @@ uint32_t block_count;
 uint32_t retardo_acceso_bloque;
 char *log_level;
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
+    int socket_cliente;
 
     levantar_config_fs();
-    logger = iniciar_logger("filesystem.config", "FILESYSTEM");
+    logger = iniciar_logger("filesystem.log", "FILESYSTEM");
 
     // HILOS PARA CONEXIONES
 
     int socket_fs = iniciar_servidor(puerto_escucha);
 
+    log_info(logger,"Servidor listo para recibir peticiones");
+
     pthread_t t1;
 
-    pthread_create(&t1, NULL, (void *)atenderMemoria, NULL);
-    
+    while (1)
+    {
+        socket_cliente = esperar_cliente(socket_fs);
+
+        //int id_modulo;
+        //recv(socket_cliente, &id_modulo, sizeof(int), 0);
+        // log_info(logger, "id_modulo: %i", id_modulo);
+        socket_fs = socket_cliente;
+        pthread_create(&t1, NULL, (void *)atenderMemoria, NULL);
+    }
+
     return 0;
 }
 
-void atenderMemoria() {
-        log_info(logger, "FS conectada con Memoria");
+void atenderMemoria()
+{
+    log_info(logger, "FS conectada con Memoria");
 }
 
-void levantar_config_fs() {
-    config_fs = iniciar_config("filesystem.config");
-    puerto_escucha = config_get_string_value(logger, "PUERTO_ESCUCHA");
-    mount_dir = config_get_string_value(logger, "MOUNT_DIR");
-    log_level = config_get_string_value(logger, "LOG_LEVEL");
-    block_size = config_get_int_value(logger, "BLOCK_SIZE");
-    block_count = config_get_int_value(logger, "BLOCK_COUNT");
-    retardo_acceso_bloque = config_get_int_value(logger, "RETARDO_ACCESO_BLOQUE");
+void levantar_config_fs()
+{
+    config_fs = config_create("filesystem.config");
+    puerto_escucha = config_get_string_value(config_fs, "PUERTO_ESCUCHA");
+    mount_dir = config_get_string_value(config_fs, "MOUNT_DIR");
+    log_level = config_get_string_value(config_fs, "LOG_LEVEL");
+    block_size = config_get_int_value(config_fs, "BLOCK_SIZE");
+    block_count = config_get_int_value(config_fs, "BLOCK_COUNT");
+    retardo_acceso_bloque = config_get_int_value(config_fs, "RETARDO_ACCESO_BLOQUE");
 }
