57f5ca791b4df654baa40f72c67acb6c0202e79b
milagrosvera <mveraoliver@frba.utn.edu.ar>
2024-09-20 23:02:09 +0000

    mutex faltante y prioridades

--- kernel/src/planificador.h
+++ kernel/src/planificador.h
@@ -1,8 +1,12 @@
 #include "utilsKernel.h"
 extern int id_counter;
+extern int indice; //permitime dudar 
 
 void pasar_a_running_tcb(tcb* tcb_listo);
 void pasar_a_running_pcb(pcb* proceso_listo);
+void pasar_a_running_tcb_prioridades();
 void planificador_corto_plazo_pcb();
-void agregar_a_ready_prioridades(tcb* hilo);
+void agregar_a_ready_prioridades(tcb* hilo); //este con lo de ahora no iria creo¿
 void agregar_a_ready(tcb* hilo);
+tcb* hilo_prioritario_en_ready();
+tcb* elegir_segun_prioridades();
